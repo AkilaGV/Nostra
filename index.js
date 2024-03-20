b80d62c01bebb17031f947e0d19fa9e1278de584
AkilaGV <akilagv1997@gmail.com>
2024-03-20 09:59:15 +0530

    updated

---
+++ index.js
@@ -0,0 +1,15 @@
+//select side navbar
+var sidenav=document.querySelector(".side-navbar")
+
+function showNavbar()
+{
+    sidenav.style.left="0"
+    
+}
+function closeNavbar()
+{
+    sidenav.style.left="-60%"
+    
+}
+
+    
